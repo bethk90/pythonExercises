3e45e659338980c62a7c2ba44ed61fd7d1ae1d0f
bethk90 <elizabethdkahn@gmail.com>
2022-05-30 10:44:26 +0100

    Add exercises

--- exercise4.py
+++ exercise4.py
@@ -1,3 +1,5 @@
+#Exercise 1: Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worked above 40 hours.
+
 hours = input("How many hours did you work? ")
 rate = input("What is your hourly rate? ")
 hours = float(hours)
@@ -14,6 +16,8 @@ print("Your pay is ",pay)
 hours = input("How many hours did you work? ")
 rate = input("What is your hourly rate? ")
 
+#Exercise 2: Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the program.
+
 try:
     hours = float(hours)
     rate = float(rate)
@@ -27,6 +31,11 @@ try:
 except:
     print("please enter numeric input")
     quit()
+
+'''
+Rewrite your pay computation with time and-a-half for overtime and create a function
+called computepay which takes two parameters (hours and rate)
+'''
 
 def computepay(hours, rate):
     if hours > 40:
