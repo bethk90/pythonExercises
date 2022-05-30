0e8de45604b1582ecd70debc3b2e0bdfe493d038
bethk90 <elizabethdkahn@gmail.com>
2022-05-30 10:20:00 +0100

    Upload exercises

---
+++ exercise2.py
@@ -0,0 +1,20 @@
+'''
+
+Exercise 2: Print the following pattern
+Write a program to print the following number pattern using a loop.
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+'''
+
+x = 1
+output = ''
+
+while x < 6:
+    output = str(output) + ' ' + str(x)
+    print(output)
+    x = x + 1
