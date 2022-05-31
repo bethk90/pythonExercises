e3c252c200aa8efcd9d444ba82fe3b504b0ec7e5
bethk90 <elizabethdkahn@gmail.com>
2022-05-31 18:36:55 +0100

    Complete guessing game exercise

--- exercise29.py
+++ exercise29.py
@@ -11,21 +11,48 @@ Keep track of how many guesses the user has taken, and when the game ends, print
 
 import random
 
-num = random.randint(0, 9)
+def guessing_game():
+    while True:
+        count = 0
+        num = random.randint(1, 9)
+        print(num)
+        usernum = input('Guess a number: ')
+        if usernum == 'exit':
+            print('Thanks for playing!')
+            break
+        try:
+            guess = int(usernum)
+        except:
+            print('Invalid data, sorry!')
+            continue
+        while True:
+            count = count + 1
+            if guess == num:
+                print(f'Correct! Thanks for playing! Guess count: {count}')
+                break
+            elif guess > num:
+                print('Too high!')
+            elif guess < num:
+                print('Too low')
+            usernum = input('Guess a number: ')
+            guess = int(usernum)
+        playagain = input('Play again Y/N? ')
+        if playagain == 'N':
+            print('Play again soon!')
+            break
 
-while True:
+guessing_game()
 
-    try:
-        usernum = int(input('Guess a number: '))
-    except:
-        print('Invalid data')
-
-    if usernum == num:
-        print('Correct!')
-        break
-
-    else:
-        if usernum > num:
-            print('Too high!')
-        elif usernum < num:
-            print('Too low!')
+ #   guess = int(usernum)
+  #  if usernum == 'break':
+  #      break
+  #  while True:
+  #      elif guess == num:
+#            print('Correct!')
+ #       elif guess > num:
+  #              print('Too high!')
+   #     elif guess < num:
+    #            print('Too low!')
+#
+ #   count = count + 1
+  #  num = random.randint(0, 9)
